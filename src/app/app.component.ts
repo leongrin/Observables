14cd176868e0b4697f977c26d552e9878830d6d1
Leon Grin <41086608+leongrin@users.noreply.github.com>
2019-03-05 15:20:56 -0300

    onDestroy added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UsersService} from './users.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   user1Activated = false;
   user2Activated = false;
+  subscription: Subscription;
 
   constructor(private userServ: UsersService) {}
 
   ngOnInit(): void {
-    this.userServ.userActivated.subscribe(
+    this.subscription = this.userServ.userActivated.subscribe(
       (id) => {
         if (id === 1) {
           this.user1Activated = true;
@@ -22,5 +24,9 @@ export class AppComponent implements OnInit {
         }
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
